ad21c6a874c3197d74e6b5807f6d81483150e5aa
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-12-19 22:41:19 +0800

    added fetch functions

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const API_KEY = "36a67f53faf163e8cc24b63c891d70c0";
+let movieTitle;
+let movieID;
 
 const getUpcomingMovies = async () => {
   try {
@@ -10,13 +12,11 @@ const getUpcomingMovies = async () => {
 
     console.log(movies);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
 getUpcomingMovies();
-
-let movieTitle;
 
 const getMovieByName = async () => {
   try {
@@ -28,6 +28,34 @@ const getMovieByName = async () => {
 
     console.log(movie);
   } catch (error) {
-    console.log(error);
+    console.error(error);
+  }
+};
+
+const getMovieInfo = async () => {
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieID}?api_key=${API_KEY}&language=en-US`
+    );
+
+    const moveInfo = await res.json();
+
+    console.log(moveInfo);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getSimilarMovies = async () => {
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieID}/similar?api_key=${API_KEY}&language=en-US&page=1`
+    );
+
+    const movies = await res.json();
+
+    console.log(movies);
+  } catch (error) {
+    console.error(error);
   }
 };
